dbf3ff1bfbdaa37ffb4937d067119e0074a283d6
thenatural86 <philip.smalls86@gmail.com>
2020-05-22 14:17:35 -0400

    image layout

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,7 +3,7 @@ import styled from "styled-components"
 
 const Img = styled.img`
   width: 100%;
-  ${"" /* height: 100%; */}
+  height: auto;
   ${"" /* object-fit: cover; */}
 `
 
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -9,15 +9,20 @@ import styled from "styled-components"
 
 const WrapperImage = styled.div`
   max-width: 80rem;
-  margin: 4rem auto;
-  ${"" /* display: grid;
-  grid-gap: 1rem;
-  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
-  grid-auto-rows: 300px; */}
-  background-color: red;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-around;
+  margin: auto;
+  ${"" /* display: grid; */}
+  ${"" /* grid-gap: 1rem; */}
+  ${"" /* grid-template-columns: repeat(auto-fill, minmax(250px, 1fr)); */}
+  ${"" /* grid-auto-rows: 300px;  */}
+  
+  ${"" /* background-color: red; */}
+  ${"" /* display: flex; */}
+  ${"" /* flex-wrap: wrap; */}
+  ${"" /* justify-content: space-around; */}
+  line-height: 1rem
+  column-count: 3;  
+  column-gap: 1rem; 
+  row-gap: 1em; 
 `
 
 export const ImageList = () => {
@@ -37,8 +42,8 @@ export const ImageList = () => {
     const apiRoot = "https://api.unsplash.com"
     const accessKey = "ByiIqQV5gReo8trB-h5T8VGRQW6EvhmyQW2EH-tLbys"
     axios
-      // .get(`${apiRoot}/photos/random?client_id=${accessKey}&count=30`)
-      .get(`${apiRoot}/photos/?client_id=${accessKey}&count=10`)
+      .get(`${apiRoot}/photos/random?client_id=${accessKey}&count=30`)
+      // .get(`${apiRoot}/photos/?client_id=${accessKey}&count=10`)
       .then((res) => setImages([...images, ...res.data]))
   }
   if (images === null) {
